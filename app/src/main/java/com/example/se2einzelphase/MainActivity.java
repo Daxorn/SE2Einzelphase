b28128fb2d71c216bcd504416c84a73d47e04b5c
Daxorn <nardone.sp@gmail.com>
2023-03-10 20:00:42 +0100

    setup main function and view

--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -3,12 +3,94 @@ package com.example.se2einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
+    //Define necessary variables
+    Socket clientSocket;
+    Button btnSend;
+    Button btnCalc;
+    EditText userInput;
+    TextView answerFromSrv;
+    TextView result;
+    String serverOutput;
+    public static final int SERVER_PORT = 53212;
+    public static final String SERVER_URL = "se2-isys.aau.at";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCalc = findViewById(R.id.btn_Calc);
+        result = findViewById(R.id.res_View);
+        btnSend = findViewById(R.id.btn_Send);
+        userInput = findViewById(R.id.inpt_ImmNumb);
+        answerFromSrv = findViewById(R.id.srvr_Ans_View);
+    }
+
+    //Sending Function
+    public void sendToServer(View view) {
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //Create connection to server
+                    clientSocket = new Socket(SERVER_URL, SERVER_PORT);
+                    //-------------------------------------------------
+
+                    //Create necessary streams inFromServer/outToServer
+                    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    //-------------------------------------------------
+
+                    //Send and receive information
+                    outToServer.writeBytes(convertToString(userInput) + '\n');
+                    serverOutput = inFromServer.readLine();
+                    //-------------------------------------
+
+                    //Set answer on view
+                    answerFromSrv.setText(serverOutput);
+                    //---------------------------------
+
+                    //Close all streams
+                    inFromServer.close();
+                    outToServer.close();
+                    clientSocket.close();
+                    //-------------------
+
+                } catch (UnknownHostException e) {
+                    serverOutput = "! Unknown Host !";
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    serverOutput = "! Server Error !";
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+    //-------------------------------------------------------------------------------------
+
+    public String convertToString(EditText userInput){
+        return userInput.getText().toString();
     }
 }
